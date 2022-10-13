8c0a2ce47510ac045bdaae3a52a585fd0b97e2e3
Damien L <damien.laneelle33@gmail.com>
2022-10-13 15:16:35 +0200

    Add front, add countries, add flags, add radios

---
+++ src/pages/About.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>À propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quos
+        minus doloribus porro tempore obcaecati neque natus expedita.
+        Praesentium, excepturi eveniet. Quibusdam porro asperiores soluta
+        officia dolores, earum inventore sit ullam ipsam quam architecto quo
+        delectus placeat commodi numquam maiores laborum omnis, nisi assumenda
+        dolorum quia? Molestias, nobis optio. Non aliquid quod vero quaerat
+        pariatur soluta veritatis natus officiis? Suscipit eaque necessitatibus
+        deserunt optio doloribus modi repellendus voluptatem in nobis. Ratione
+        consectetur quasi pariatur fuga corporis modi recusandae libero incidunt
+        doloribus eius eum, unde deserunt architecto quaerat eaque et ex
+        assumenda voluptatibus porro reprehenderit, necessitatibus quod omnis
+        autem. Voluptas, eveniet?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur
+        iusto, eius exercitationem tempore, molestiae nulla quaerat voluptatem
+        hic sequi ex facere inventore, repellendus numquam! Ipsa nemo illum
+        debitis sunt illo? Iste nobis aliquid accusamus eligendi asperiores,
+        blanditiis eos, cum et consequatur mollitia doloribus, veritatis veniam
+        voluptas molestiae laboriosam incidunt laudantium?
+      </p>
+    </div>
+  );
+};
+
+export default About;
